99f55ce00d33aecc3a512155bb8f7c264363aad0
Phoenix <zengphoenix@live.com>
2022-06-24 23:28:13 +0800

    .

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -9,7 +9,6 @@ module.exports = {
     title: '内置Japi文档',
     description: 'embedded-japi-vue-press',
     evergreen: true,
-    base: "/japi/",
     plugins: plugin,
     markdown: {
         lineNumbers: true,
@@ -32,5 +31,24 @@ module.exports = {
         searchMaxSuggestions: 20,
         displayAllHeaders: true, // 默认值：false
         sidebar: sidebar,
+        // github
+        // 假定是 GitHub. 同时也可以是一个完整的 GitLab URL
+        repo: 'PhoenixZeng/embedded-japi-vue-press',
+        // 自定义仓库链接文字。默认从 `themeConfig.repo` 中自动推断为
+        // "GitHub"/"GitLab"/"Bitbucket" 其中之一，或是 "Source"。
+        repoLabel: '查看源码',
+
+        // 以下为可选的编辑链接选项
+
+        // 假如你的文档仓库和项目本身不在一个仓库：
+        docsRepo: 'PhoenixZeng/embedded-japi-vue-press',
+        // 假如文档不是放在仓库的根目录下：
+        docsDir: 'docs',
+        // 假如文档放在一个特定的分支下：
+        docsBranch: 'main',
+        // 默认是 false, 设置为 true 来启用
+        editLinks: true,
+        // 默认为 "Edit this page"
+        editLinkText: '在GitHub上编辑'
     }
 }
